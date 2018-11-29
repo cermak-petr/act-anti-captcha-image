458bf6aac95e1e1aaf474a52ecfef3855d10d12a
Petr Čermák <30902390+cermak-petr@users.noreply.github.com>
2018-11-29 18:54:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
+const uril = require('util');
 const Apify = require('apify');
-const Promise = require('bluebird');
 const i2b = require('node-base64-image');
 const request = require('request-promise');
-Apify.setPromisesDependency(Promise);
 
-const i2bp = Promise.promisify(i2b.encode);
+const i2bp = util.promisify(i2b.encode);
 
 class Anticaptcha {
     
